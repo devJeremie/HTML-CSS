81ff35545e62a049160cb3c4143e8ea7c6b38a04
devJeremie <chabanaisjeremie@gmail.com>
2024-01-11 09:28:52 +0100

    Exo SJ DWWM

--- Javascript/index.js
+++ Javascript/index.js
@@ -8,3 +8,15 @@ basculant.addEventListener('click', function (event) {
 // 4. Toggle the 'active' class on the '.menuBurger' element.
 menuBurger.classList.toggle('active')
 })
+
+class Etudiant {
+    constructor(age) {
+        this.age = age;
+    }
+}
+
+// instanciation d'objets Etudiant
+var etudiant1 = new Etudiant(17);
+
+// afficher l'age de etudiant1 dans la balise <div>
+document.getElementById("resultat").innerHTML = "etudiant1 a " + etudiant1.age;
