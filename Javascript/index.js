8ecd4a117e6856cac711bba488635f1f184f95c2
devJeremie <chabanaisjeremie@gmail.com>
2026-01-06 09:32:04 +0100

    MAj-HtmlCSS

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,32 +1,32 @@
 // // // Récupère le texte du h1 avec l'id "titre"
-// let hello = document.getElementById('titre').textContent;
+let hello = document.getElementById('titre').textContent;
 // // // Affiche une alerte avec ce texte
-// alert(hello);
+alert(hello);
 
 
 // // 1. Select the '.menuBurger' element from the DOM.
-// let menuBurger = document.querySelector('.menuBurger'),
+let menuBurger = document.querySelector('.menuBurger'),
 // // 2. Select the '.basculant' element from the DOM.
-// basculant =  document.querySelector('.basculant')
+basculant =  document.querySelector('.basculant')
 
 // // 3. Add a 'click' event listener to the '.basculant' element.
-// basculant.addEventListener('click', function () {
+basculant.addEventListener('click', function () {
 // // 4. Toggle the 'active' class on the '.menuBurger' element.
-// menuBurger.classList.toggle('active')
-// });
+menuBurger.classList.toggle('active')
+});
 
 
 
 // // POP UP // 
 // // Affiche la pop-up automatiquement au chargement de la page
-// window.onload = function() {
-//   document.getElementById('popup').classList.add('active');
-// };
+window.onload = function() {
+    document.getElementById('popup').classList.add('active');
+};
 
 // // Ferme la pop-up au clic sur la croix
-// document.getElementById('closeBtn').onclick = function() {
-//   document.getElementById('popup').classList.remove('active');
-// };
+document.getElementById('closeBtn').onclick = function() {
+   document.getElementById('popup').classList.remove('active');
+};
 
 
 // class Etudiant {
