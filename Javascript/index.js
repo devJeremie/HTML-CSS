78f7e3443b0e5b57b853e76f37671ae9b36839c4
devJeremie <chabanaisjeremie@gmail.com>
2026-06-02 10:43:29 +0200

    MAj

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,19 +1,19 @@
-// // // // Récupère le texte du h1 avec l'id "titre"
-// let hello = document.getElementById('titre').textContent;
-// // // // Affiche une alerte avec ce texte
-// alert(hello);
+// // // // // Récupère le texte du h1 avec l'id "titre"
+// // let hello = document.getElementById('titre').textContent;
+// // // // // Affiche une alerte avec ce texte
+// // alert(hello);
 
 
-// // 1. Select the '.menuBurger' element from the DOM.
-let menuBurger = document.querySelector('.menuBurger'),
-// // 2. Select the '.basculant' element from the DOM.
-basculant =  document.querySelector('.basculant')
+// // // 1. Select the '.menuBurger' element from the DOM.
+// let menuBurger = document.querySelector('.menuBurger'),
+// // // 2. Select the '.basculant' element from the DOM.
+// basculant =  document.querySelector('.basculant')
 
-// // 3. Add a 'click' event listener to the '.basculant' element.
-basculant.addEventListener('click', function () {
-// // 4. Toggle the 'active' class on the '.menuBurger' element.
-menuBurger.classList.toggle('active')
-});
+// // // 3. Add a 'click' event listener to the '.basculant' element.
+// basculant.addEventListener('click', function () {
+// // // 4. Toggle the 'active' class on the '.menuBurger' element.
+// menuBurger.classList.toggle('active')
+// });
 
 
 
